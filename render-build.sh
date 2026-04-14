a4748561f905397e6f527a27442d32e6d25ac637
arunkampilla <arun.consultant7@gmail.com>
2026-04-13 23:57:07 -0500

    update code

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,8 @@
 # Build frontend
-cd frontend && npm install && npm run build
+cd src/frontend/ && npm install && npm run build
 # Move build files to backend static folder
-cp -r dist/* ../backend/
+cp -r dist/* ..src/backend/
 # Install backend dependencies
-cd ../backend && pip install -r requirements.txt
+cd src/backend && pip install -r requirements.txt
+# Start commadn
+cd src/backend && uvicorn server:app --host 0.0.0.0 --port $PORT
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
+
