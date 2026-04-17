4646efab8821a7402a3001cc9935d8f785b74aee
arunkampilla <arun.consultant7@gmail.com>
2026-04-16 23:53:01 -0500

    update code

--- server.js
+++ server.js
@@ -5,13 +5,17 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+const port = process.env.PORT || 8081;
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
+
+
+//const PORT = process.env.PORT || 5000;
+//
+//app.listen(PORT, () => {
+//  console.log("Server running on port", PORT);
+//});
 
 
 app.get('/*any', (req, res) => {
