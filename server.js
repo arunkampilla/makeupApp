a1344cd2dae621d4e31e38b309078c0bf4472dfa
arunkampilla <arun.consultant7@gmail.com>
2026-04-16 23:36:09 -0500

    update code

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
+
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
+});
+
+
+app.get('/*any', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
