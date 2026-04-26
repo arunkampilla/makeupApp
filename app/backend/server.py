5d0aff3ac709064a897dc455eee16e457cbd13da
arunkampilla <arun.consultant7@gmail.com>
2026-04-26 14:43:38 -0500

    Update server.py

--- app/backend/server.py
+++ app/backend/server.py
@@ -335,3 +335,7 @@ logger = logging.getLogger(__name__)
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
+    
+@app.get("/")
+async def root():
+    return {"status": "FASTAPI RUNNING"}
