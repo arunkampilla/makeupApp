009ad7c85cffc1006e152e8abf5d3ec58489d866
arunkampilla <arun.consultant7@gmail.com>
2026-04-30 23:51:15 -0500

    moved code

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,10 @@
 # Build frontend
-cd app/ && npm install && npm run build
+cd app/frontend && npm install && npm run build
 # Move build files to backend static folder
 cp -r dist/* ..app/backend/
 # Install backend dependencies
 cd app/backend && pip install -r requirements.txt
 # Start command
-uvicorn app.backend.server:app --host 0.0.0.0 --port 10000
+uvicorn server:app --host 0.0.0.0 --port $PORT
+#uvicorn app.backend.server:app --host 0.0.0.0 --port 10000
 #cd app/backend && uvicorn server:app --host 0.0.0.0 --port $PORT
